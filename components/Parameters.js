003a6930267ac2878a075e9e4a0a63b5db6c18e5
Borys Generalov <borys.generalov@microsoft.com>
2021-06-03 22:21:05 +0200

    cleanup parameters and variables

--- components/Parameters.js
+++ components/Parameters.js
@@ -8,40 +8,10 @@ import { generateMermaidDiagram } from '../helpers/mermaidDiagram';
  * Always when you see something useful could be added to the API of parsed AsyncAPI document, please create an issue 
  * https://github.com/asyncapi/parser-js/issues
  */
-export function Parameters({ asyncapi }) {
-  if (asyncapi.hasComponents() && asyncapi.components().hasSchemas()) {
-    return schemasContent(asyncapi);
-  }
-  return fallback();
-}
-
-function schemasContent(asyncapi) {
-  const schemasList = Object.keys(asyncapi.components().schemas())
-    .sort()
-    .map(schemaName => {
-      return `<li><a href="schemas/${schemaName}.html">${schemaName}</a></li>`;
-    }).join('');
-
+export function Parameters({ asyncapi }) {  
   return `
-<h2>Schemas</h2>
-<hr>
-You have the following schemas named in components section:
-<div class="container mx-auto px-8">
-  <ul class="list-disc">${schemasList}</ul>
-</div>
-<div class="mermaid">
-  ${generateMermaidDiagram(asyncapi)}
-</div>
-`;
+{
+  
 }
-
-function fallback() {
-  return `
-You have no schemas in components sections. It most probably means that you have them directly under payload without using references. 
-This is not a good practice because this way you cannot name your schemas, and we cannot generate a nice meaningfull diagram for you basing on schemas with anonymous name.
-<br><br>
-Sorry about that. Come back to us once you restructure your document.
-<br><br>
-<img src="sample.gif" alt="no go gif" width="200" height="200">  
 `;
 }
--- components/Variables.js
+++ components/Variables.js
@@ -2,26 +2,10 @@
  * Here you can see example of complex operations. 
  * You can actually do whatever you want. It is important that the value returned from the function must be a string or a component.
  */
-export function Variables({ channels, operationType = 'publish' }) {
-  const namesList = Object.entries(channels)
-    .map(([channelName, channel]) => {
-      if (
-        (operationType === 'publish' && channel.hasPublish()) || 
-        (operationType === 'subscribe' && channel.hasSubscribe())
-      ) {
-        return  `<li><strong>${channelName}</strong></li>`;
-      }
-    })
-    .filter(Boolean);
-
+export function Variables({ asyncapi }) {
   return `
-<h2>
-  Channels that you can ${operationType} to
-</h2>
-<hr />
-<br />
-<div class="container mx-auto px-8">
-  <ul class="list-disc">${namesList.join('')}</ul>
-</div>
+{
+  
+}
 `;
 }
